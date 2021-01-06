01fd477afe2880c6998cff959ca1d26b0be6eee9
ferdyerlangga <ferdyerlangga7889@gmail.com>
2021-01-06 12:10:11 +0700

    admin

--- application/views/back/layouts/_sidebar.php
+++ application/views/back/layouts/_sidebar.php
@@ -1,57 +1,55 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-	<!-- Sidebar - Brand -->
-	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('admin') ?>">
-		<div class="sidebar-brand-icon">
-			<i class="fab fa-artstation"></i>
-		</div>
-		<div class="sidebar-brand-text mx-2">Admin SMK ASY-Syuhada</div>
-	</a>
-
-	<!-- Divider -->
-	<hr class="sidebar-divider my-0">
-
-	<!-- Nav Item - Dashboard -->
-	<li class="nav-item active">
-		<a class="nav-link" href="<?= base_url('admin') ?>">
-			<i class="fas fa-fw fa-tachometer-alt"></i>
-			<span>Dashboard</span></a>
-	</li>
-
-	<?php 
-		$menus = $this->menu->getMenu();
-
-		foreach($menus as $menu) :
-			$submenu = $this->menu->getSubmenu($menu->id);
-	?>
-
-	<li class="nav-item">
-		<?php if($submenu) : ?>
-			<a class="nav-link collapsed" href="<?= base_url() ?>" data-toggle="collapse" data-target="#collapse<?= $menu->id ?>" aria-expanded="true" aria-controls="collapse">
-				<i class="<?= $menu->icon ?>"></i>
-				<span><?= $menu->title ?></span>
-			</a>
-
-			<div id="collapse<?= $menu->id ?>" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-				<div class="bg-white py-2 collapse-inner rounded">
-				<?php foreach($submenu as $sm) : ?>
-					<a class="collapse-item" href="<?= base_url() . $sm->sub_url ?>"><?= $sm->sub_title ?></a>
-				<?php endforeach ?>
-				</div>
-			</div>
-		<?php else : ?>
-			<a class="nav-link" href="<?= base_url() . $menu->url ?>">
-				<i class="<?= $menu->icon ?>"></i>
-				<span><?= $menu->title ?></span>
-			</a>
-		<?php endif; ?>		
-	</li>
-
-	<?php endforeach ?>
-
-	<!-- Sidebar Toggler (Sidebar) -->
-	<div class="text-center d-none d-md-inline">
-		<button class="rounded-circle border-0" id="sidebarToggle"></button>
+<div class="sidebar" data-color="green">
+	<!--
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+    -->
+	<div class="logo">
+		<a href="http://www.creative-tim.com" class="simple-text logo-mini">
+			ADMIN
+		</a>
+		<a href="http://www.creative-tim.com" class="simple-text logo-normal">
+			SMK KESDA BERGAS
+		</a>
 	</div>
+	<div class="sidebar-wrapper" id="sidebar-wrapper">
+		<ul class="nav">
+			<li>
+				<a href="<?= base_url('admin') ?>">
+					<i class="now-ui-icons design_app"></i>
+					<p>Dashboard</p>
+				</a>
+			</li>
 
-</ul>
+			<?php
+			$menus = $this->menu->getMenu();
+
+			foreach ($menus as $menu) :
+				$submenu = $this->menu->getSubmenu($menu->id);
+			?>
+
+				<li class="nav-item">
+					<?php if ($submenu) : ?>
+						<a class="nav-link collapsed" href="<?= base_url() ?>" data-toggle="collapse" data-target="#collapse<?= $menu->id ?>" aria-expanded="true" aria-controls="collapse">
+							<i class="<?= $menu->icon ?>"></i>
+							<span><?= $menu->title ?></span>
+						</a>
+
+						<div id="collapse<?= $menu->id ?>" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+							<div class="bg-white py-2 collapse-inner rounded">
+								<?php foreach ($submenu as $sm) : ?>
+									<a class="collapse-item" href="<?= base_url() . $sm->sub_url ?>"><?= $sm->sub_title ?></a>
+								<?php endforeach ?>
+							</div>
+						</div>
+					<?php else : ?>
+						<a class="nav-link" href="<?= base_url() . $menu->url ?>">
+							<i class="<?= $menu->icon ?>"></i>
+							<span><?= $menu->title ?></span>
+						</a>
+					<?php endif; ?>
+				</li>
+
+			<?php endforeach ?>
+
+		</ul>
+	</div>
+</div>
--- application/views/back/layouts/main.php
+++ application/views/back/layouts/main.php
@@ -2,31 +2,25 @@
 <html lang="en">
 
 <head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>Admin - <?= $title ?></title>
-
-  <!-- Custom fonts for this template-->
-  <link href="<?= base_url() ?>asset/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Datatables -->
-  <link href="<?= base_url() ?>asset/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-
-	<!-- Summernote -->
-  <link href="<?= base_url() ?>asset/vendor/summernote/dist/summernote-bs4.min.css" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="<?= base_url() ?>asset/css/sb-admin-2.min.css" rel="stylesheet">
-
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url() ?>/asset/admin_assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="<?= base_url() ?>/asset/admin_assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Now UI Dashboard by Creative Tim
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <!-- CSS Files -->
+  <link href="<?= base_url() ?>/asset/admin_assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="<?= base_url() ?>/asset/admin_assets/css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="<?= base_url() ?>/asset/admin_assets/demo/demo.css" rel="stylesheet" />
 </head>
 
-<body id="page-top">
+<body class="">
 
   <!-- Page Wrapper -->
   <div id="wrapper">
@@ -35,37 +29,23 @@
     <?php $this->load->view('back/layouts/_sidebar') ?>
     <!-- End of Sidebar -->
 
-    <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column">
+    <!-- Topbar Navbar -->
+    <?php $this->load->view('back/layouts/_topbar') ?>
+    </nav>
+    <!-- End of Topbar -->
 
-      <!-- Main Content -->
-      <div id="content">
+    <!-- Begin Page Content -->
+    <?php $this->load->view('back/pages/' . $page) ?>
 
-        <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+  </div>
+  <!-- End of Main Content -->
 
-          <!-- Sidebar Toggle (Topbar) -->
-          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-          </button>
+  <!-- Footer -->
+  <?php $this->load->view('back/layouts/_footer') ?>
+  <!-- End of Footer -->
 
-          <!-- Topbar Navbar -->
-    		<?php $this->load->view('back/layouts/_topbar') ?>
-        </nav>
-        <!-- End of Topbar -->
-
-			<!-- Begin Page Content -->
-			<?php $this->load->view('back/pages/' . $page) ?>
-
-      </div>
-      <!-- End of Main Content -->
-
-		<!-- Footer -->
-		<?php $this->load->view('back/layouts/_footer') ?>
-		<!-- End of Footer -->
-
-    </div>
-    <!-- End of Content Wrapper -->
+  </div>
+  <!-- End of Content Wrapper -->
 
   </div>
   <!-- End of Page Wrapper -->
@@ -94,60 +74,57 @@
     </div>
   </div>
 
-   <!-- Bootstrap core JavaScript-->
-   <script src="<?= base_url() ?>asset/vendor/jquery/jquery.min.js"></script>
-	<script src="<?= base_url() ?>asset/vendor/popper/popper.min.js"></script>
-   <script src="<?= base_url() ?>asset/vendor/bootstrap/js/bootstrap.min.js"></script>
+  <!--   Core JS Files   -->
+  <script src="<?= base_url() ?>/asset/admin_assets/js/core/jquery.min.js"></script>
+  <script src="<?= base_url() ?>/asset/admin_assets/js/core/popper.min.js"></script>
+  <script src="<?= base_url() ?>/asset/admin_assets/js/core/bootstrap.min.js"></script>
+  <script src="<?= base_url() ?>/asset/admin_assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Chart JS -->
+  <script src="<?= base_url() ?>/asset/admin_assets/js/plugins/chartjs.min.js"></script>
+  <!--  Notifications Plugin    -->
+  <script src="<?= base_url() ?>/asset/admin_assets/js/plugins/bootstrap-notify.js"></script>
+  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="<?= base_url() ?>/asset/admin_assets/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+  <script src="<?= base_url() ?>/asset/admin_assets/demo/demo.js"></script>
+  <script>
+    $(document).ready(function() {
+      // Javascript method's body can be found in assets/js/demos.js
+      demo.initDashboardPageCharts();
 
-   <!-- Core plugin JavaScript-->
-   <script src="<?= base_url() ?>asset/vendor/jquery-easing/jquery.easing.min.js"></script>
+    });
+  </script>
+  <script>
+    $('#summernote').summernote({
+      height: 300,
+      toolbar: [
+        // [groupName, [list of button]]
+        ['style', ['bold', 'italic', 'underline', 'clear', 'fontname']],
+        ['misc', ['undo', 'redo']],
+        ['font', ['strikethrough', 'superscript', 'subscript']],
+        ['fontsize', ['fontsize']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['height', ['height']],
+        ['insert', ['table', 'link', 'hr', 'fullscreen']],
+      ],
+      placeholder: 'Masukkan konten berita disini...'
+    });
+  </script>
 
-   <!-- Datatables -->
-   <script src="<?= base_url() ?>asset/vendor/datatables/jquery.dataTables.min.js"></script>
-   <script src="<?= base_url() ?>asset/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+  <!-- For Datatable -->
+  <?php
+  if (isset($datatable)) {
+    $this->load->view('back/pages/' . $datatable);
+  }
+  ?>
 
-   <!-- Custom scripts for all pages-->
-   <script src="<?= base_url() ?>asset/js/sb-admin-2.min.js"></script>
-
-   <!-- Page level plugins -->
-   <script src="<?= base_url() ?>asset/vendor/chart.js/Chart.min.js"></script>
-
-	 <!-- Summernote -->
-   <script src="<?= base_url() ?>asset/vendor/summernote/dist/summernote-bs4.min.js"></script>
-
-	 <!-- Sweet Alert 2 -->
-   <script src="<?= base_url() ?>asset/vendor/sweetalert2/sweetalert2.js"></script>
-
-	<script>
-      $('#summernote').summernote({
-         height: 300,
-			toolbar: [
-				// [groupName, [list of button]]
-				['style', ['bold', 'italic', 'underline', 'clear', 'fontname']],
-				['misc', ['undo', 'redo']],
-				['font', ['strikethrough', 'superscript', 'subscript']],
-				['fontsize', ['fontsize']],
-				['color', ['color']],
-				['para', ['ul', 'ol', 'paragraph']],
-				['height', ['height']],
-				['insert', ['table', 'link', 'hr', 'fullscreen']],
-			],
-			placeholder: 'Masukkan konten berita disini...'
-      });
-   </script>
-
-	<!-- For Datatable -->
-   <?php 
-      if(isset($datatable)){
-         $this->load->view('back/pages/'. $datatable);
-      }
-   ?>
-
-   <!-- For Chart in Dashboard -->
-  <?php 
-    if(isset($pageChart)){
-      $this->load->view('back/layouts/'. $pageChart);
-    }
+  <!-- For Chart in Dashboard -->
+  <?php
+  if (isset($pageChart)) {
+    $this->load->view('back/layouts/' . $pageChart);
+  }
   ?>
 
 </body>
