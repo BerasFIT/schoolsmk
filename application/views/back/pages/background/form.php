9ea442aeb4bc4eae18b350179bbbbbe2004a8a4f
ferdyerlangga <ferdyerlangga7889@gmail.com>
2021-01-17 12:45:37 +0700

    form update admin, hal utama

--- application/views/back/pages/background/form.php
+++ application/views/back/pages/background/form.php
@@ -1,38 +1,48 @@
 <div class="container">
-	<div class="row mb-4">
-		<div class="col">
-			<h3>Form Background Login</h3>
+	<div class="col-xl-12 col-lg-12">
+		<div class="card shadow mb-4">
+			<!-- Card Header - Dropdown -->
+			<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+				<h4 class="m-0 font-weight-bold text-success">Form Update Background Login</h4>
+			</div>
+			<!-- Card Body -->
+			<div class="card-body">
+				<div class="row mb-4">
+					<div class="col">
+					</div>
+				</div>
+
+				<?= form_open_multipart($form_action) ?>
+				<?= isset($content->id) ? form_hidden('id', $content->id) : '' ?>
+
+				<div class="form-group row">
+					<label class="col-sm-2 col-form-label" id="label-photo">Foto</label>
+					<div class="col-sm-8">
+						<?php if (!empty($content->photo)) : ?>
+							<img src="<?= base_url("img/background/$content->photo") ?>" height="150">
+						<?php else : ?>
+							<p>No Photo</p>
+						<?php endif; ?>
+						<br>
+						<small><span class="text-danger">*</span> Maksimal ukuran gambar adalah 3 MB</small>
+						<br> <br>
+						<input name="photo" type="file" class="form-control-file">
+						<?php if ($this->session->flashdata('image_error')) :  ?>
+							<small class="form-text text-danger">
+								<?= $this->session->flashdata('image_error') ?>
+							</small>
+						<?php endif ?>
+					</div>
+				</div>
+
+				<div class="row mt-4">
+					<div class="col-8 offset-2">
+						<a href="<?= base_url('background') ?>" class="btn btn-sm btn-secondary"><i class="fas fa-angle-left mr-1"></i>Kembali</a>
+						<button type="submit" class="btn btn-sm btn-success float-right"><i class="fas fa-check mr-1"></i> Simpan</button>
+					</div>
+				</div>
+				<?= form_close() ?>
+			</div>
 		</div>
 	</div>
-
-	<?= form_open_multipart($form_action) ?>
-		<?= isset($content->id) ? form_hidden('id', $content->id) : '' ?>
-
-		<div class="form-group row">
-			<label class="col-sm-2 col-form-label" id="label-photo">Foto</label>
-			<div class="col-sm-8">
-				<?php if(!empty($content->photo)) : ?>
-					<img src="<?= base_url("img/background/$content->photo") ?>" height="150">
-				<?php else: ?>
-					<p>No Photo</p>
-				<?php endif; ?>
-				<br> 
-				<small><span class="text-danger">*</span>	Maksimal ukuran gambar adalah 3 MB</small>
-				<br> <br>
-				<input name="photo" type="file" class="form-control-file">
-				<?php if($this->session->flashdata('image_error')) :  ?>
-                <small class="form-text text-danger">
-                  <?= $this->session->flashdata('image_error') ?>
-                </small>
-				<?php endif ?>
-			</div>
-		</div>
-
-		<div class="row mt-4">
-			<div class="col-8 offset-2">
-				<a href="<?= base_url('background') ?>" class="btn btn-sm btn-secondary"><i class="fas fa-angle-left mr-1"></i>Kembali</a>
-				<button type="submit" class="btn btn-sm btn-primary float-right"><i class="fas fa-check mr-1"></i> Simpan</button>
-			</div>
-		</div>
-	<?= form_close() ?>
 </div>
--- application/views/back/pages/identitas/form.php
+++ application/views/back/pages/identitas/form.php
@@ -1,42 +1,57 @@
 <div class="container">
-	<div class="row mb-4">
-		<div class="col">
-			<h3>Form Update Identitas Website</h3>
-		</div>
-	</div>
-
-	<?= form_open_multipart($form_action) ?>
-		<?= isset($content->id) ? form_hidden('id', $content->id) : '' ?>
-
-		<div class="form-group row">
-			<label for="meta_description" class="col-sm-2 col-form-label">Deskripsi</label>
-			<div class="col-sm-8">
-				<textarea name="meta_description" id="meta_description" rows="5" class="form-control" value="<?= $content->meta_description ?>"><?= $content->meta_description ?></textarea>
-				<?= form_error('meta_description', '<small class="form-text text-danger">', '</small>') ?>
+	<div class="col-xl-12 col-lg-12">
+		<div class="card shadow mb-4">
+			<!-- Card Header - Dropdown -->
+			<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+				<h4 class="m-0 font-weight-bold text-success">Form Update Identitas Website</h4>
+				<?php if ($this->session->flashdata('success')) : ?>
+					<div class="alert alert-success alert-dismissible fade show" role="alert">
+						<?= $this->session->flashdata('success') ?>
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+				<?php endif ?>
 			</div>
-		</div>
+			<!-- Card Body -->
+			<div class="card-body">
+				<div class="row mb-4">
+					<div class="col">
+					</div>
+				</div>
 
-		<div class="form-group row">
-			<label for="meta_keyword" class="col-sm-2 col-form-label">Keyword</label>
-			<div class="col-sm-8">
-				<textarea name="meta_keyword" id="meta_keyword" rows="5" class="form-control" value="<?= $content->meta_keyword ?>"><?= $content->meta_keyword ?></textarea>
-				<?= form_error('meta_keyword', '<small class="form-text text-danger">', '</small>') ?>
-			</div>
-		</div>
+				<?= form_open_multipart($form_action) ?>
+				<?= isset($content->id) ? form_hidden('id', $content->id) : '' ?>
 
-		<!-- <div class="form-group row">
+				<div class="form-group row">
+					<label for="meta_description" class="col-sm-2 col-form-label">Deskripsi</label>
+					<div class="col-sm-8">
+						<textarea name="meta_description" id="meta_description" rows="5" class="form-control" value="<?= $content->meta_description ?>"><?= $content->meta_description ?></textarea>
+						<?= form_error('meta_description', '<small class="form-text text-danger">', '</small>') ?>
+					</div>
+				</div>
+
+				<div class="form-group row">
+					<label for="meta_keyword" class="col-sm-2 col-form-label">Keyword</label>
+					<div class="col-sm-8">
+						<textarea name="meta_keyword" id="meta_keyword" rows="5" class="form-control" value="<?= $content->meta_keyword ?>"><?= $content->meta_keyword ?></textarea>
+						<?= form_error('meta_keyword', '<small class="form-text text-danger">', '</small>') ?>
+					</div>
+				</div>
+
+				<!-- <div class="form-group row">
 			<label class="col-sm-2 col-form-label" id="label-photo">Foto</label>
 			<div class="col-sm-8">
-				<?php if(!empty($content->photo)) : ?>
+				<?php if (!empty($content->photo)) : ?>
 					<img src="<?= base_url("img/identitas/$content->photo") ?>" alt="" height="150">
-				<?php else: ?>
+				<?php else : ?>
 					<p>No Photo</p>
 				<?php endif; ?>
 				<br> 
 				<small><span class="text-danger">*</span>	Maksimal ukuran gambar adalah 1 MB</small>
 				<br> <br>
 				<input name="photo" type="file" class="form-control-file">
-				<?php if($this->session->flashdata('image_error')) :  ?>
+				<?php if ($this->session->flashdata('image_error')) :  ?>
                 <small class="form-text text-danger">
                   <?= $this->session->flashdata('image_error') ?>
                 </small>
@@ -44,11 +59,14 @@
 			</div>
 		</div> -->
 
-		<div class="row mt-4">
-			<div class="col-8 offset-2">
-				<a href="<?= base_url('identitas') ?>" class="btn btn-sm btn-secondary"><i class="fas fa-angle-left mr-1"></i>Kembali</a>
-				<button type="submit" class="btn btn-sm btn-primary float-right"><i class="fas fa-check mr-1"></i> Simpan</button>
+				<div class="row mt-4">
+					<div class="col-8 offset-2">
+						<a href="<?= base_url('identitas') ?>" class="btn btn-sm btn-secondary"><i class="fas fa-angle-left mr-1"></i>Kembali</a>
+						<button type="submit" class="btn btn-sm btn-success float-right"><i class="fas fa-check mr-1"></i> Simpan</button>
+					</div>
+				</div>
+				<?= form_close() ?>
 			</div>
 		</div>
-	<?= form_close() ?>
+	</div>
 </div>
