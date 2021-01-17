e83613a92391af610c88d024dd23d9a53d85c3e5
ferdyerlangga <ferdyerlangga7889@gmail.com>
2021-01-17 13:47:35 +0700

    done admin

--- application/views/auth/create_user.php
+++ application/views/auth/create_user.php
@@ -30,94 +30,95 @@
 
                         <!-- Begin Page Content -->
                         <div class="container">
-                              <div class="row">
-                                    <div class="col">
-                                          <h1><?php echo lang('create_user_heading'); ?></h1>
-                                          <p><?php echo lang('create_user_subheading'); ?></p>
+                              <div class="col-xl-12 col-lg-12">
+                                    <div class="card shadow mb-4">
+                                          <!-- Card Header - Dropdown -->
+                                          <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                                <h4 class="m-0 font-weight-bold text-success"><?php echo lang('create_user_heading'); ?></h4><br>
+
+                                          </div>
+
+                                          <div id="infoMessage"><?php echo $message; ?></div>
+
+                                          <?php echo form_open("auth/create_user"); ?>
+                                          <div class="card-body">
+                                                <div class="form-row mt-2">
+                                                      <div class="form-group ">
+                                                            <?php echo lang('create_user_fname_label', 'first_name'); ?> <br />
+                                                            <?php echo form_input($first_name); ?>
+                                                      </div>
+                                                </div>
+                                                <div class="form-row mt-2">
+                                                      <div class="form-group">
+                                                            <?php echo lang('create_user_lname_label', 'last_name'); ?> <br />
+                                                            <?php echo form_input($last_name); ?>
+                                                      </div>
+                                                </div>
+                                                <?php
+                                                if ($identity_column !== 'email') {
+                                                      echo '<p>';
+                                                      echo lang('create_user_identity_label', 'identity');
+                                                      echo '<br />';
+                                                      echo form_error('identity');
+                                                      echo form_input($identity);
+                                                      echo '</p>';
+                                                }
+                                                ?>
+
+                                                <div class="form-row mt-2">
+                                                      <div class="form-group">
+                                                            <?php echo lang('create_user_company_label', 'company'); ?> <br />
+                                                            <?php echo form_input($company); ?>
+                                                      </div>
+                                                </div>
+
+                                                <div class="form-row mt-2">
+                                                      <div class="form-group">
+                                                            <?php echo lang('create_user_email_label', 'email'); ?> <br />
+                                                            <?php echo form_input($email); ?>
+                                                      </div>
+                                                </div>
+
+                                                <div class="form-row mt-2">
+                                                      <div class="form-group">
+                                                            <?php echo lang('create_user_phone_label', 'phone'); ?> <br />
+                                                            <?php echo form_input($phone); ?>
+                                                      </div>
+                                                </div>
+
+                                                <div class="form-row mt-2">
+                                                      <div class="form-group">
+                                                            <?php echo lang('create_user_password_label', 'password'); ?> <br />
+                                                            <?php echo form_input($password); ?>
+                                                      </div>
+                                                </div>
+
+                                                <div class="form-row mt-2">
+                                                      <div class="form-group">
+                                                            <?php echo lang('create_user_password_confirm_label', 'password_confirm'); ?> <br />
+                                                            <?php echo form_input($password_confirm); ?>
+                                                      </div>
+                                                </div>
+
+                                                <div class="row mt-2">
+                                                      <div class="col">
+                                                            <?php echo form_submit('submit', lang('create_user_submit_btn'), ['class' => 'btn btn-success']); ?>
+
+                                                      </div>
+                                                </div>
+
+                                                <?php echo form_close(); ?>
+                                          </div>
+
                                     </div>
+                                    <!-- End of Main Content -->
+
+                                    <!-- Footer -->
+                                    <?php $this->load->view('back/layouts/_footer') ?>
+                                    <!-- End of Footer -->
+
                               </div>
+                              <!-- End of Content Wrapper -->
 
-                              <div id="infoMessage"><?php echo $message; ?></div>
-
-                              <?php echo form_open("auth/create_user"); ?>
-
-                              <div class="form-row mt-2">
-                                    <div class="form-group ">
-                                          <?php echo lang('create_user_fname_label', 'first_name'); ?> <br />
-                                          <?php echo form_input($first_name); ?>
-                                    </div>
-                              </div>
-                              <div class="form-row mt-2">
-                                    <div class="form-group">
-                                          <?php echo lang('create_user_lname_label', 'last_name'); ?> <br />
-                                          <?php echo form_input($last_name); ?>
-                                    </div>
-                              </div>
-                              <?php
-                              if ($identity_column !== 'email') {
-                                    echo '<p>';
-                                    echo lang('create_user_identity_label', 'identity');
-                                    echo '<br />';
-                                    echo form_error('identity');
-                                    echo form_input($identity);
-                                    echo '</p>';
-                              }
-                              ?>
-
-                              <div class="form-row mt-2">
-                                    <div class="form-group">
-                                          <?php echo lang('create_user_company_label', 'company'); ?> <br />
-                                          <?php echo form_input($company); ?>
-                                    </div>
-                              </div>
-
-                              <div class="form-row mt-2">
-                                    <div class="form-group">
-                                          <?php echo lang('create_user_email_label', 'email'); ?> <br />
-                                          <?php echo form_input($email); ?>
-                                    </div>
-                              </div>
-
-                              <div class="form-row mt-2">
-                                    <div class="form-group">
-                                          <?php echo lang('create_user_phone_label', 'phone'); ?> <br />
-                                          <?php echo form_input($phone); ?>
-                                    </div>
-                              </div>
-
-                              <div class="form-row mt-2">
-                                    <div class="form-group">
-                                          <?php echo lang('create_user_password_label', 'password'); ?> <br />
-                                          <?php echo form_input($password); ?>
-                                    </div>
-                              </div>
-
-                              <div class="form-row mt-2">
-                                    <div class="form-group">
-                                          <?php echo lang('create_user_password_confirm_label', 'password_confirm'); ?> <br />
-                                          <?php echo form_input($password_confirm); ?>
-                                    </div>
-                              </div>
-
-                              <div class="row mt-2">
-                                    <div class="col">
-                                          <?php echo form_submit('submit', lang('create_user_submit_btn'), ['class' => 'btn btn-success']); ?>
-
-                                    </div>
-                              </div>
-
-                              <?php echo form_close(); ?>
                         </div>
-
-                  </div>
-                  <!-- End of Main Content -->
-
-                  <!-- Footer -->
-                  <?php $this->load->view('back/layouts/_footer') ?>
-                  <!-- End of Footer -->
-
-            </div>
-            <!-- End of Content Wrapper -->
-
-      </div>
-      <!-- End of Page Wrapper -->
+                        <!-- End of Page Wrapper -->
