9b3c04d16eb9f03d21f1e8680f70b7008c1b474d
ferdyerlangga <ferdyerlangga7889@gmail.com>
2021-01-17 15:08:57 +0700

    Update _footer.php

--- application/views/front/layouts/_footer.php
+++ application/views/front/layouts/_footer.php
@@ -1,24 +1,54 @@
-<div class="section section-download" id="download-section" data-background-color="black">
-	<div class="container">
-		<div class="row text-center">
-			<div class="col-md-auto ml-auto mr-auto">
-				<img class="n-logo" src="<?= base_url() ?>/asset/landing_assets/img/smk-logo.png" width="150px">
+<footer class="footer bg-dark text-light">
+	<div class="container bottom_border">
+		<div class="row">
+			<div class=" col-sm-4 col-md col-sm-4  col-12 col">
+				<h5 class="headin5_amrc col_white_amrc pt2">Hubungi Kami</h5>
+				<hr class="bg-light">
+				<!--headin5_amrc-->
+				<p class="mb10">SMK KESEHATAN DARUSSALAM BERGAS</p>
+				<p><i class="fa fa-map-marker"></i> JALAN SYEKH PENANGGALAN NO.1 Semarang, Jawa Tengah 50552 </p>
+				<p><i class="fa fa-phone"></i> (024) 6926938 </p>
+				<p><i class="fa fa fa-envelope"></i> info@example.com </p>
+
+
 			</div>
-		</div>
-		<div class="row justify-content-md-center sharing-area text-center">
-			<div class="text-center col-md-12 col-lg-8">
-				<h3>SMK KESEHATAN DARUSSALAM BERGAS</h3>
+
+
+			<div class=" col-sm-4 col-md  col-6 col">
+				<h5 class="headin5_amrc col_white_amrc pt2">Tentang Kami</h5>
+				<hr class="bg-light">
+				<!--footer-->
+				<p><a class="text-white" href="<?= base_url('profil/sejarah') ?>">Sejarah</a></p>
+				<p><a class="text-white" href="<?= base_url('profil/visimisi') ?>">Visi & Misi</a></p>
+				<p><a class="text-white" href="<?= base_url('profil/struktur') ?>">Struktur Organisasi</a></p>
+				<p><a class="text-white" href="<?= base_url('profil/fasilitas') ?>">Fasilitas</a></p>
+				<!--footer ends here-->
 			</div>
+
+
+			<div class=" col-sm-4 col-md  col-6 col">
+				<h5 class="headin5_amrc col_white_amrc pt2">Ikuti Kami</h5>
+				<hr class="bg-light">
+				<!--footer-->
+				<p><a class="text-white" href="https://www.facebook.com/SMK-Kesehatan-Darussalam-Bergas-102229018460582" target="_blank"><i class="fab fa-facebook-f"></i> Facebook</a></p>
+				<p><a class="text-white" href="https://www.instagram.com/smkkesehatandarussalam/" target="_blank"><i class="fab fa-instagram"></i> Instagram</a></p>
+				<p><a class="text-white" href="https://twitter.com/smkkesda1" target="_blank"><i class="fab fa-twitter"></i> Twitter</a></p>
+				<p><a class="text-white" href="https://www.youtube.com/channel/UCyIpBMCIZ8SHSnXItUOAOqQ" target="_blank"><i class="fab fa-youtube"></i> Youtube</a></p>
+
+				<!--footer ends here-->
+			</div>
+
+
 		</div>
 	</div>
-</div>
-</div>
-<footer class="footer" data-background-color="black">
+
+
 	<div class="container">
-		<div class="copyright" id="copyright">
-			&copy;
-			<script>
-				document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-			</script> | Designed by SMK KESEHATAN DARUSSALAM BERGAS
-		</div>
+		<p class="text-center mt-4">
+			Copyright &copy;<script>
+				document.write(new Date().getFullYear());
+			</script> <i class="fa fa-heart-o" aria-hidden="true"></i> by <a class="text-success" target="_blank">SMK KESEHATAN DARUSSALAM BERGAS</a> All rights reserved.
+		</p>
+	</div>
+
 </footer>
