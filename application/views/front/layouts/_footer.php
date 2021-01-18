c238454ab82f467e38eb27048c08860de2f1f004
ferdyerlangga <ferdyerlangga7889@gmail.com>
2021-01-18 12:23:00 +0700

    Update _footer.php

--- application/views/front/layouts/_footer.php
+++ application/views/front/layouts/_footer.php
@@ -1,22 +1,17 @@
 <footer class="footer bg-dark text-light">
 	<div class="container bottom_border">
 		<div class="row">
-			<div class=" col-sm-4 col-md col-sm-4  col-12 col">
-				<h5 class="headin5_amrc col_white_amrc pt2">Hubungi Kami</h5>
-				<hr class="bg-light">
+			<div class="col-sm-4 col-md-5">
+				<h5 class="headin5_amrc col_white_amrc pt2"><b>Hubungi Kami</b></h5>
+				<hr class="bg-success">
 				<!--headin5_amrc-->
-				<p class="mb10">SMK KESEHATAN DARUSSALAM BERGAS</p>
 				<p><i class="fa fa-map-marker"></i> JALAN SYEKH PENANGGALAN NO.1 Semarang, Jawa Tengah 50552 </p>
 				<p><i class="fa fa-phone"></i> (024) 6926844 </p>
 				<p><i class="fa fa fa-envelope"></i> smkkesehatandarussalam@yahoo.com</p>
-
-
 			</div>
-
-
-			<div class=" col-sm-4 col-md  col-6 col">
-				<h5 class="headin5_amrc col_white_amrc pt2">Tentang Kami</h5>
-				<hr class="bg-light">
+			<div class="col-sm-4 col-md-4">
+				<h5 class="headin5_amrc col_white_amrc pt2"><b>Tentang Kami</b></h5>
+				<hr class="bg-success">
 				<!--footer-->
 				<p><a class="text-white" href="<?= base_url('profil/sejarah') ?>">Sejarah</a></p>
 				<p><a class="text-white" href="<?= base_url('profil/visimisi') ?>">Visi & Misi</a></p>
@@ -24,25 +19,18 @@
 				<p><a class="text-white" href="<?= base_url('profil/fasilitas') ?>">Fasilitas</a></p>
 				<!--footer ends here-->
 			</div>
-
-
-			<div class=" col-sm-4 col-md  col-6 col">
-				<h5 class="headin5_amrc col_white_amrc pt2">Ikuti Kami</h5>
-				<hr class="bg-light">
+			<div class=" col-sm-4 col-md-3">
+				<h5 class="headin5_amrc col_white_amrc pt2"><b>Ikuti Kami</b></h5>
+				<hr class="bg-success">
 				<!--footer-->
 				<p><a class="text-white" href="https://www.facebook.com/SMK-Kesehatan-Darussalam-Bergas-102229018460582" target="_blank"><i class="fab fa-facebook-f"></i> Facebook</a></p>
 				<p><a class="text-white" href="https://www.instagram.com/smkkesehatandarussalam/" target="_blank"><i class="fab fa-instagram"></i> Instagram</a></p>
 				<p><a class="text-white" href="https://twitter.com/smkkesda1" target="_blank"><i class="fab fa-twitter"></i> Twitter</a></p>
 				<p><a class="text-white" href="https://www.youtube.com/channel/UCyIpBMCIZ8SHSnXItUOAOqQ" target="_blank"><i class="fab fa-youtube"></i> Youtube</a></p>
-
 				<!--footer ends here-->
 			</div>
-
-
 		</div>
 	</div>
-
-
 	<div class="container">
 		<p class="text-center mt-4">
 			Copyright &copy;<script>
@@ -50,5 +38,4 @@
 			</script> <i class="fa fa-heart-o" aria-hidden="true"></i> by <a class="text-success" target="_blank">SMK KESEHATAN DARUSSALAM BERGAS</a> All rights reserved.
 		</p>
 	</div>
-
 </footer>
