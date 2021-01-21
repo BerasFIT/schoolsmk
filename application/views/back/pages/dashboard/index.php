986c7d5a8cdc6ffd45aaecfe6dc5db1d6b359c22
ferdyerlangga <ferdyerlangga7889@gmail.com>
2021-01-21 11:04:40 +0700

    Update index.php

--- application/views/back/pages/dashboard/index.php
+++ application/views/back/pages/dashboard/index.php
@@ -98,23 +98,23 @@
 
    <!-- Content Row -->
 
-   <div class="row">
+   <!-- <div class="row"> -->
 
-      <div class="col-xl-12 col-lg-12">
-         <div class="card shadow mb-4">
-            <!-- Card Header - Dropdown -->
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-               <h6 class="m-0 font-weight-bold text-success">Total Postingan Bulanan</h6>
-            </div>
-            <!-- Card Body -->
-            <div class="card-body">
-               <div class="chart-area">
-                  <canvas id="myAreaChart" height="100vh"></canvas>
-               </div>
-            </div>
-         </div>
-      </div>
+   <!-- <div class="col-xl-12 col-lg-12"> -->
+   <!-- <div class="card shadow mb-4"> -->
+   <!-- Card Header - Dropdown -->
+   <!-- <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between"> -->
+   <!-- <h6 class="m-0 font-weight-bold text-success">Total Postingan Bulanan</h6> -->
+   <!-- </div> -->
+   <!-- Card Body -->
+   <!-- <div class="card-body"> -->
+   <!-- <div class="chart-area"> -->
+   <!-- <canvas id="myAreaChart"></canvas> -->
+   <!-- </div> -->
+   <!-- </div> -->
+   <!-- </div> -->
+   <!-- </div> -->
 
-   </div>
+   <!-- </div> -->
 
 </div>
